c48d0f2c91a0454ed13ba469ba9fd87d27a20ae7
Sardor2625 <ganiyevsardor222@gmail.com>
2023-12-05 19:55:00 +0500

    books update 3

--- src/views/admin/BooksView.vue
+++ src/views/admin/BooksView.vue
@@ -3,50 +3,44 @@
     <div class="card">
       <div class="card-header">
         <h1>Books</h1>
-    <input type="text" v-model="searchQuery" placeholder="Search...">
-    <select v-model="selectedCategory" @change="filterBooks">
-      <option value="">All Categories</option>
-      <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
-    </select>
-    <input type="text" v-model="selectedLanguage" placeholder="Language" @input="filterBooks">
-    <input type="text" v-model="selectedPublicationYear" placeholder="Publication Year" @input="filterBooks">
-    <input type="text" v-model="searchByAuthor" placeholder="Author" @input="filterBooks">
-    <input type="number" v-model="selectedCopiesOwned" placeholder="Copies Owned" @input="filterBooks">
-  </div>
-  
-      <div class="card-body ">
-
+        <input type="text" v-model="searchQuery" placeholder="Search...">
+        <select v-model="selectedCategory" @change="filterBooks">
+          <option value="">All Categories</option>
+          <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+        </select>
+        <input type="text" v-model="selectedLanguage" placeholder="Language" @input="filterBooks">
+        <input type="text" v-model="selectedPublicationYear" placeholder="Publication Year" @input="filterBooks">
+        <input type="text" v-model="searchByAuthor" placeholder="Author" @input="filterBooks">
+        <input type="number" v-model="selectedCopiesOwned" placeholder="Copies Owned" @input="filterBooks">
+      </div>
+      
+      <div class="card-body">
         <table class="table table-bordered">
           <thead>
-          <tr>
-            <th>ID</th>
-            <th>title</th>
-            <th>publicationYear</th>
-            <th>language</th>
-            <th>categoryId</th>
-            <th>copiesOwned</th>
-
-            <!-- <th>Count</th> -->
-            <!-- <th> <b-button class="btn btn-primary" size="sm">Refresh</b-button></th> -->
-
-          </tr>
-
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Publication Year</th>
+              <th>Language</th>
+              <th>Category ID</th>
+              <th>Copies Owned</th>
+              <th>Actions</th>
+            </tr>
           </thead>
-          <tbody >
-          <tr v-for="(book, index) in this.books" :key="index">
-            <td>{{ book.id }}</td>
-            <td>{{ book.title }}</td>
-            <td>{{ book.publicationYear }}</td>
-            <td>{{ book.language }}</td>
-            <td>{{ book.categoryId }}</td>
-            <td>{{ book.copiesOwned }}</td>
-            <td>
-            <button @click="editBook(book)">Edit</button>
-            <button @click="deleteBook(book.id)">Delete</button>
-          </td>
-          </tr>
+          <tbody>
+            <tr v-for="(book, index) in filteredBooks" :key="index">
+              <td>{{ book.id }}</td>
+              <td>{{ book.title }}</td>
+              <td>{{ book.publicationYear }}</td>
+              <td>{{ book.language }}</td>
+              <td>{{ book.categoryId }}</td>
+              <td>{{ book.copiesOwned }}</td>
+              <td>
+                <button @click="editBook(book)">Edit</button>
+                <button @click="deleteBook(book.id)">Delete</button>
+              </td>
+            </tr>
           </tbody>
-          
         </table>
       </div>
     </div>
@@ -60,25 +54,41 @@ export default {
   name: 'books',
   data() {
     return {
-      books: []
+      books: [],
+      categories: [], // Assuming you have categories data
+      searchQuery: '',
+      selectedCategory: '',
+      selectedLanguage: '',
+      selectedPublicationYear: '',
+      searchByAuthor: '',
+      selectedCopiesOwned: null,
     }
   },
   mounted() {
-    this.getbooks()
-    // this.getStudents();
-    // console.log('i am here')
+    this.getBooks()
+    // Fetch categories if needed
   },
   methods: {
-    getbooks() {
+    getBooks() {
       axios.get("http://127.0.0.1:5208/api/Book")
-          .then(res => {
-            this.books = res.data;
-            console.log(res.data)
-          })
-    }
-  },
-  
-  editBook(book) {
+        .then(res => {
+          this.books = res.data;
+          console.log(res.data)
+        })
+    },
+    filterBooks() {
+      this.filteredBooks = this.books.filter(book => {
+        const matchesCategory = !this.selectedCategory || book.categoryId === parseInt(this.selectedCategory);
+        const matchesLanguage = !this.selectedLanguage || book.language.toLowerCase().includes(this.selectedLanguage.toLowerCase());
+        const matchesPublicationYear = !this.selectedPublicationYear || book.publicationYear.toString() === this.selectedPublicationYear;
+        const matchesAuthor = !this.searchByAuthor || book.author.toLowerCase().includes(this.searchByAuthor.toLowerCase());
+        const matchesCopiesOwned = !this.selectedCopiesOwned || book.copiesOwned === parseInt(this.selectedCopiesOwned);
+
+        return matchesCategory && matchesLanguage && matchesPublicationYear && matchesAuthor && matchesCopiesOwned;
+      });
+    },
+    
+    editBook(book) {
       // Implement logic to handle editing a book
       axios.put(`api/books/${book.id}`, book)
         .then(response => {
@@ -100,8 +110,10 @@ export default {
           console.error('Error deleting book:', error);
         });
     },
-  }
-
+  },
+};
 </script>
 
-<!-- sdgfdshjgsdghdfsg -->
+<style>
+/* Add any custom styles here */
+</style>
