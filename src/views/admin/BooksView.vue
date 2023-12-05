6267fd206d81eee2ca255adfa37cdd2d52bb9e53
Sardor2625 <ganiyevsardor222@gmail.com>
2023-12-05 19:35:48 +0500

    books update 2

--- src/views/admin/BooksView.vue
+++ src/views/admin/BooksView.vue
@@ -2,11 +2,18 @@
   <div class="container">
     <div class="card">
       <div class="card-header">
-        <h4>
-          Books
-        </h4>
-
-      </div>
+        <h1>Books</h1>
+    <input type="text" v-model="searchQuery" placeholder="Search...">
+    <select v-model="selectedCategory" @change="filterBooks">
+      <option value="">All Categories</option>
+      <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+    </select>
+    <input type="text" v-model="selectedLanguage" placeholder="Language" @input="filterBooks">
+    <input type="text" v-model="selectedPublicationYear" placeholder="Publication Year" @input="filterBooks">
+    <input type="text" v-model="searchByAuthor" placeholder="Author" @input="filterBooks">
+    <input type="number" v-model="selectedCopiesOwned" placeholder="Copies Owned" @input="filterBooks">
+  </div>
+  
       <div class="card-body ">
 
         <table class="table table-bordered">
@@ -25,7 +32,7 @@
           </tr>
 
           </thead>
-          <tbody v-if="this.books.length > 0">
+          <tbody >
           <tr v-for="(book, index) in this.books" :key="index">
             <td>{{ book.id }}</td>
             <td>{{ book.title }}</td>
@@ -33,13 +40,13 @@
             <td>{{ book.language }}</td>
             <td>{{ book.categoryId }}</td>
             <td>{{ book.copiesOwned }}</td>
+            <td>
+            <button @click="editBook(book)">Edit</button>
+            <button @click="deleteBook(book.id)">Delete</button>
+          </td>
           </tr>
           </tbody>
-          <!-- <tbody v-else>
-              <tr>
-                  <td colspan="6">Loading</td>
-              </tr>
-          </tbody> -->
+          
         </table>
       </div>
     </div>
@@ -70,5 +77,31 @@ export default {
           })
     }
   },
-}
+  
+  editBook(book) {
+      // Implement logic to handle editing a book
+      axios.put(`api/books/${book.id}`, book)
+        .then(response => {
+          console.log('Book updated:', response.data);
+          // Perform any necessary actions after successful update
+        })
+        .catch(error => {
+          console.error('Error updating book:', error);
+        });
+    },
+    deleteBook(bookId) {
+      // Implement logic to handle deleting a book
+      axios.delete(`api/books/${bookId}`)
+        .then(response => {
+          console.log('Book deleted:', response.data);
+          // Perform any necessary actions after successful deletion
+        })
+        .catch(error => {
+          console.error('Error deleting book:', error);
+        });
+    },
+  }
+
 </script>
+
+<!-- sdgfdshjgsdghdfsg -->
